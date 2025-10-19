691d115c5a11c3bb9a204c2d1a8ca34eaeea3107
Ishant2464 <ishant2464@gmail.com>
2025-10-20 03:52:07 +0530

    Solved Line Trip

---
+++ CP-31 SHEET/800 R/Line Trip.cpp
@@ -0,0 +1,26 @@
+// Problem: Line Trip
+// Link: https://codeforces.com/problemset/problem/1901/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+        int ans = a[0];
+        for(int i = 1; i < n; i++) {
+            ans = max(ans, a[i] - a[i -1]);
+        }
+        ans = max(ans, 2*(x - a[n - 1]));
+        cout << ans << endl;
+    }
+    return 0;
+}
+
