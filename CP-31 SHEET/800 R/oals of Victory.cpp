e19a020316013f3ac6357c03ad69f9dfc70a0d43
Ishant2464 <ishant2464@gmail.com>
2025-10-25 22:33:22 +0530

    Solved Goals of Victory

---
+++ CP-31 SHEET/800 R/oals of Victory.cpp
@@ -0,0 +1,44 @@
+// Problem: Goals of Victory
+// Link: https://codeforces.com/problemset/problem/1877/A
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> a(n - 1);
+    int sum = 0;
+    for(int i = 0; i < n - 1; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    cout << -sum << "\n";
+}
+
+/**
+ * This is the main function where the program starts.
+ */
+int main() {
+    // 1. FAST I/O: This speeds up cin and cout dramatically.
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // 2. FILE I/O: This block only runs on your local machine.
+#ifndef ONLINE_JUDGE
+    // Redirects input from "input.txt"
+    freopen("input.txt", "r", stdin);
+    // Redirects output to "output.txt"
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
