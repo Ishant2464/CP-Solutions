ca957a32ca4a3a257f48eddb2d5da2e8ac631e95
Ishant2464 <ishant2464@gmail.com>
2025-12-04 23:54:30 +0530

    Solved Odd Grasshopper

--- CP-31 SHEET/900 R/Odd Grasshopper.cpp
+++ CP-31 SHEET/900 R/Odd Grasshopper.cpp
@@ -4,28 +4,36 @@
 using namespace std;
 
 void solve() {
-    long long start, jumps; // 'start' is the initial position, 'jumps' is the number of jumps
-		cin >> start >> jumps;
-
-		long long final_pos; // Variable to store the final position after all jumps
-
-		// Determine the effect of the number of jumps on the position
-		if (jumps % 4 == 1)
-			final_pos = -jumps; // If jumps % 4 == 1, move left by 'jumps'
-		else if (jumps % 4 == 2)
-			final_pos = 1; // If jumps % 4 == 2, move right by 1
-		else if (jumps % 4 == 3)
-			final_pos = jumps + 1; // If jumps % 4 == 3, move right by 'jumps + 1'
-		else if (jumps % 4 == 0)
-			final_pos = 0; // If jumps % 4 == 0, no net movement
-
-		// Adjust the final position based on the parity of the starting position
-		if (start % 2 == 0)
-			final_pos = start + final_pos; // If starting position is even, add the calculated position
-		else
-			final_pos = start - final_pos; // If starting position is odd, subtract the calculated position
-
-		cout << final_pos << endl;
+	long long x, n;
+	cin >> x >> n;
+	if(x % 2 == 0) {
+		if(n % 4 == 0) {
+			cout << x << "\n";
+		}
+		else if(n % 4 == 1) {
+			cout << x - n << "\n";
+		}
+		else if(n % 4 == 2) {
+			cout << x + 1 << "\n";
+		}
+		else {
+			cout << x + 1 + n << "\n";
+		}
+	}
+	else {
+		if(n % 4 == 0) {
+			cout << x << "\n";
+		}
+		else if(n % 4 == 1) {
+			cout << x + n << "\n";
+		}
+		else if(n % 4 == 2) {
+			cout << x - 1 << "\n";
+		}
+		else {
+			cout << x - 1 - n << "\n";
+		}
+	}
 }
 
 int main() {
